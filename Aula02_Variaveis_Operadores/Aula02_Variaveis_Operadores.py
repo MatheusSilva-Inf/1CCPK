b3fe5c54bd72073d30bc87e6a651ffa1a3db78f1
MatheusSilva-Inf <matheus_matheus26@hotmail.com>
2026-03-16 09:53:42 -0300

    Aula 02: Variáveis

---
+++ Aula02_Variaveis_Operadores/Aula02_Variaveis_Operadores.py
@@ -0,0 +1,43 @@
+print("Teste")
+
+''' comentários 
+    de multiplas 
+    linhas'''
+'''
+
+Variáveis e input básico
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = int(input("Digite seu peso: "))
+
+print ("O nome do usuário é: ",nome)
+print ("A idade do usuário é: ",idade)
+print ("O peso do usuário é: ",peso)
+'''
+
+'''
+# Desafio 1 - criar um script que leia o nome e mostre uma mensagem de boas vindas
+
+nome = input("Por favor, insira seu nome: ")
+print(f"Seja bem-vindo, {nome}, obrigado pelo acesso")
+'''
+
+'''
+# Desafio 2 - criar um script que leia o dia, o mês e o ano de nascimento e escreva uma mensagem com a data
+
+nascimento_dia = int(input("Por favor, insira seu dia de nascimento: "))
+nascimento_mes = int(input("Por favor, insira seu mês de nascimento: "))
+nascimento_ano = int(input("Por favor, insira seu ano de nascimento: "))
+print(f"Você nasceu em {nascimento_dia}/{nascimento_mes}/{nascimento_ano}, obrigado pelo acesso")
+'''
+
+'''
+# Desafio 3 - criar um script que leia dois números e mostre a soma
+
+num1 = int(input("Por favor, insira um número aleatório: "))
+num2 = int(input("Por favor, insira um segundo  número aleatório: "))
+print(f"a soma dos seu números aleatórios é: {num2+num1}, obrigado pelo acesso")
+'''
+
+
